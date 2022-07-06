c433f93614b3722c7222be05596e9c9a1dceba8d
asaadevCecile <asaaky@kakao.com>
2022-07-06 18:48:47 +0200

    convertisseur poids

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Convertisseur d'unités</title>
+    <meta charset="utf-8">
+    <!-- CSS -->
+    <link rel="stylesheet" href="stylepoid.css">
+    <!--  JS  -->
+    <script src="script.js"></script>
+    <script src="jspoid.js"></script>
+</head>
+
+<body>
+    <h2>Convertisseur de poids :</h2>
+    <div class="form-group">
+        <label>Grammes</label>
+        <input id="grammes" type="number" placeholder="Grammes">
+    </div>
+    <div class="form-group">
+        <label>Kilos</label>
+        <input id="kilos" type="number" placeholder="Kilos">
+    </div>
+    <div class="form-group">
+        <label>Livres</label>
+        <input id="livres" type="number" placeholder="livres">
+    </div>
+</body>
+
+</html>
